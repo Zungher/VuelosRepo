c6dd2860b30707414d3d4f0e3f85ab6add119b1a
Zungher <71110304+Zungher@users.noreply.github.com>
2025-03-22 02:01:36 -0600

    Update vuelos.js

--- api/vuelos.js
+++ api/vuelos.js
@@ -1,3 +1,4 @@
+// api/salidas.js (por ejemplo)
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
@@ -5,8 +6,14 @@ module.exports = async (req, res) => {
 
   try {
     const response = await fetch(url);
-    const html = await response.text();
+    let html = await response.text();
 
+    // Reemplaza la ruta original de imágenes por tu ruta local
+    // Si en el HTML original aparece <img src="/vuelos/pantallas/img/ua.png">
+    // lo convertimos en <img src="/images/ua.png">
+    html = html.replace(/\/vuelos\/pantallas\/img\//g, '/images/');
+
+    // Configuramos cabeceras y enviamos el HTML modificado
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Content-Type', 'text/html');
     res.status(200).send(html);
